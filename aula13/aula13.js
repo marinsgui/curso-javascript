f9ef01a91f00fc6eda9a6f6e61a8f2bf0efcb008
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-08-28 12:11:11 -0300

    Update aula13.js

---
+++ aula13/aula13.js
@@ -0,0 +1,7 @@
+let umaString = 'O rato roeu a roupa do rei de roma.'
+
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
+console.log(umaString.slice(2, 6));
+console.log(umaString.indexOf('u'));
+console.log(umaString.charAt(7));
