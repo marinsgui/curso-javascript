567db7686b85b92efb5b561f82f0c7f316845875
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-08-29 13:43:28 -0300

    Update script.js

--- aula15/ex/js/script.js
+++ aula15/ex/js/script.js
@@ -4,8 +4,8 @@ const texto = document.querySelector('#texto');
 
 numeroTitulo.innerHTML = `${numero}`;
 texto.innerHTML = `<p>Raiz quadrada: ${Math.sqrt(numero)}</p>
-<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>
-<p>É NaN: ${Number.isNaN(numero)}</p>
-<p>Arredondando para baixo: ${Math.floor(numero)}</p>
-<p>Arredondando para cima: ${Math.ceil(numero)}</p>
-<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`
+                <p>${numero} é inteiro: ${Number.isInteger(numero)}</p>
+                <p>É NaN: ${Number.isNaN(numero)}</p>
+                <p>Arredondando para baixo: ${Math.floor(numero)}</p>
+                <p>Arredondando para cima: ${Math.ceil(numero)}</p>
+                <p>Com duas casas decimais: ${numero.toFixed(2)}</p>`
