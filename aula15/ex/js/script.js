5580831c6697f4fb9c75f9a7c2a25e04d3921f3c
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-08-29 12:56:41 -0300

    novos exercicios

---
+++ aula15/ex/js/script.js
@@ -0,0 +1,11 @@
+const numero = Number(prompt('Digite um número:'));
+const numeroTitulo = document.querySelector('#numero-titulo');
+const texto = document.querySelector('#texto');
+
+numeroTitulo.innerHTML = `${numero}`;
+texto.innerHTML = `<p>Raiz quadrada: ${Math.sqrt(numero)}</p>
+<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>
+<p>É NaN: ${Number.isNaN(numero)}</p>
+<p>Arredondando para baixo: ${Math.floor(numero)}</p>
+<p>Arredondando para cima: ${Math.ceil(numero)}</p>
+<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`
