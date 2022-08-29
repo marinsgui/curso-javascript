bc0a83df1a98ad46a956260b6b375379eafb75e2
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-08-29 12:04:26 -0300

    Create index.js

---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+// let num1 = 9.49;
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+
+// console.log(Math.max(1,2,3,4,5,10,65,48,5000,2,15648,-5,-4,-458,-852147))
+// console.log(Math.min(1,2,3,4,5,10,65,48,5000,2,15648,-5,-4,-458,-852147))
+// console.log(num2);
+
+const aleatorio = Math.round(Math.random() * (1 - 10) + 10);
+console.log(aleatorio);
