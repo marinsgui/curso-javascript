3c4589359b220178a21e414fe903901077a54282
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-08-30 10:43:08 -0300

    Create index.js

---
+++ aula16/index.js
@@ -0,0 +1,17 @@
+const alunos = ['João', 'Maria', 'Leo'];
+
+
+
+// alunos.shift(); // Remove do começo
+
+// alunos.pop(); // Remove do fim
+
+// alunos.unshift('Julia'); // Adiciona no começo
+
+// alunos[3] = 'Diego'; // Adiciona na posição selecionada
+
+// alunos.push('Eduardo'); // Adiciona no fim
+
+// alunos[alunos.length] = 'Aline'; // Adiciona no fim
+
+console.log(alunos);
