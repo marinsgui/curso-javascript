c5a4a348793e97727c365f63e47a8699318417fd
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-09-06 10:01:26 -0300

    Create index.js

---
+++ aula37/index.js
@@ -0,0 +1,9 @@
+const nome = ['Guilherme', 'Marins', 'Ferreira', 'de', 'Oliveira'];
+
+for (let valor of nome) {
+    console.log(valor)
+}
+
+// for clássico - geralmente com iteráveis (arrays ou strings)
+// for in - retorna o índice ou chave (string, array ou objeto)
+// for of - retorna o valor em si (iteráveis, arrays ou strings)
