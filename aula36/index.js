f2eee4a1a562af5b011c9e461bb827119888551e
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-09-06 09:46:55 -0300

    Create index.js

---
+++ aula36/index.js
@@ -0,0 +1,15 @@
+// const frutas = ['Pêra', 'Maçã', 'Uva'];
+
+// for (let indice in frutas) {
+//     console.log(frutas[indice])
+// }
+
+const pessoa = {
+    nome: 'Guilherme',
+    sobrenome: 'Marins',
+    idade: 25
+}
+
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave])
+}
