949e422c51451762e9b58ea10f159e7efcd6b602
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-09-08 11:17:17 -0300

    Create index.js

---
+++ aula49/index.js
@@ -0,0 +1,21 @@
+function falaOi() {
+    console.log('Oi');
+}
+falaOi();
+
+const souUmDado = function() {
+    console.log('Sou um dado.');
+}
+souUmDado();
+
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+}
+funcaoArrow();
+
+const obj = {
+    falar() {
+        console.log('Sou uma função dentro de um objeto.');
+    }
+}
+obj.falar();
