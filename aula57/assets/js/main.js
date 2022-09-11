df45056ab783ecd5759d502900bd7bd83c9ab399
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-09-11 13:13:10 -0300

    Update main.js

--- aula57/assets/js/main.js
+++ aula57/assets/js/main.js
@@ -3,7 +3,35 @@ function criaCalculadora() {
         display: document.querySelector('.display'),
         btnClear: document.querySelector('.btn-clear'),
 
+        inicia() {
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
 
+        pressionaEnter() {
+            this.display.addEventListener('keyup', (e) => {
+                if(e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            })
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+            
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta inválida!')
+                    return;
+                }
+
+                this.display.value = conta;
+            } catch (e) {
+                alert('Conta inválida!');
+            }
+        },
 
         clearDisplay() {
             this.display.value = '';
@@ -13,9 +41,6 @@ function criaCalculadora() {
             this.display.value = this.display.value.slice(0, -1);
         },
 
-        inicia() {
-            this.cliqueBotoes();
-        },
 
         cliqueBotoes() {
             document.addEventListener('click', function(e) {
@@ -31,6 +56,10 @@ function criaCalculadora() {
 
                 if(el.classList.contains('btn-del')) {
                     this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
                 }
             }.bind(this))
         },
