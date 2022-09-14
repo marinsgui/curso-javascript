b611ce29540dfd2cc8cdfa4c2a0b2f1723027efd
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-09-14 10:21:05 -0300

    Create index.js

---
+++ aula66/index.js
@@ -0,0 +1,26 @@
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// const numerosEmDobro = numeros.map(valor => valor * 2);
+
+// console.log(numerosEmDobro);
+
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 }
+];
+
+const nomes = pessoas.map(obj => obj.nome);
+const idades = pessoas.map(obj => ({ idade: obj.idade }));
+const comIds = pessoas.map(function(obj, indice) {
+    const newObj = {...obj};
+    newObj.id = (indice + 1) * 1000;
+    return newObj;
+})
+
+console.log(nomes);
+console.log(idades);
+console.log(comIds);
