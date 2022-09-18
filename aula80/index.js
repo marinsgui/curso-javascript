2d918dfc3bd6128460505d98a8903647afb82ce7
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-09-18 09:51:30 -0300

    Create index.js

---
+++ aula80/index.js
@@ -0,0 +1,25 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    falar() {
+        console.log(`${this.nome} está falando.`);
+    }
+
+    comer() {
+        console.log(`${this.nome} está comendo.`);
+    }
+
+    beber() {
+        console.log(`${this.nome} está bebendo.`);
+    }
+}
+
+const p1 = new Pessoa('Guilherme', 'Marins');
+
+console.log(p1);
+p1.falar();
+p1.comer();
+p1.beber();
